63e506b4744c05fde42d00cb21c17f549f602e7e
Mehedi0101 <mehedi29121998@gmail.com>
2023-11-04 23:20:41 +0600

    added open sans font and favicon

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'primary': ['"Open Sans"', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#FF6B6B'
+      },
+    },
+    plugins: [],
+  }
+}
