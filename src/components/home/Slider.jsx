626851afca3ebb9e90983fbf8aa072888d3632bd
Mehedi0101 <mehedi29121998@gmail.com>
2023-11-05 18:53:31 +0600

    navbar made responsive

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -21,7 +21,7 @@ const Slider = () => {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 4000,
+                    delay: 10000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
@@ -36,9 +36,9 @@ const Slider = () => {
                         <div className="hero-overlay bg-opacity-60 bg-[#1C1C1C]"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-lg">
-                                <h5 className='text-xs tracking-widest font-light mb-2'>TASTY AND CRUNCHY</h5>
-                                <h1 className="mb-5 text-6xl font-bold">Savor Culinary Excellence</h1>
-                                <p className="mb-5">Indulge in a world of flavors as we craft dishes that tell stories on every plate. Join us for an unforgettable dining experience.</p>
+                                <h5 className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>TASTY AND CRUNCHY</h5>
+                                <h1 className="mb-5 xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-bold">Savor Culinary Excellence</h1>
+                                <p className="mb-5 lg:text-base text-sm font-medium">Embark on a gastronomic journey that transcends the ordinary. Our expert chefs blend passion and innovation, creating culinary masterpieces that delight the senses.</p>
                                 <button onClick={handleViewMenu} className="px-5 py-2 font-semibold text-sm bg-primary text-white hover:bg-white hover:text-primary active:scale-95 transition-all">VIEW MENU</button>
                             </div>
                         </div>
@@ -50,9 +50,9 @@ const Slider = () => {
                         <div className="hero-overlay bg-opacity-60 bg-[#1C1C1C]"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-lg">
-                                <h5 className='text-xs tracking-widest font-light mb-2'>ROBUST AND DELICATE</h5>
-                                <h1 className="mb-5 text-6xl font-bold">Discover Unforgettable Moments</h1>
-                                <p className="mb-5">Our restaurant is where memories are made, and every meal is an opportunity to create something beautiful. Come share those moments with us.</p>
+                                <h5 className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>ROBUST AND DELICATE</h5>
+                                <h1 className="mb-5 xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-bold">Discover Unforgettable Moments</h1>
+                                <p className="mb-5 lg:text-base text-sm font-medium">Our restaurant is where memories are made, and every meal is an opportunity to create something beautiful. Come share those moments with us.</p>
                                 <button onClick={handleViewMenu} className="px-5 py-2 font-semibold text-sm bg-primary text-white hover:bg-white hover:text-primary active:scale-95 transition-all">VIEW MENU</button>
                             </div>
                         </div>
@@ -64,9 +64,9 @@ const Slider = () => {
                         <div className="hero-overlay bg-opacity-60 bg-[#1C1C1C]"></div>
                         <div className="hero-content text-center text-neutral-content">
                             <div className="max-w-lg">
-                                <h5 className='text-xs tracking-widest font-light mb-2'>LUSH AND AROMATIC</h5>
-                                <h1 className="mb-5 text-6xl font-bold">Elevate Your Senses</h1>
-                                <p className="mb-5">At <span className='font-bold'>CHEF&apos;S DOMAIN</span> , we believe in the power of taste and presentation. Immerse yourself in an extraordinary culinary journey that awakens your senses.</p>
+                                <h5 className='lg:text-xs text-[10px] tracking-widest font-light mb-2'>LUSH AND AROMATIC</h5>
+                                <h1 className="mb-5 xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-bold">Elevate Your Senses</h1>
+                                <p className="mb-5 lg:text-base text-sm font-medium">At <span className='font-bold'>CHEF&apos;S DOMAIN</span> , we believe in the power of taste and presentation. Immerse yourself in an extraordinary culinary journey that awakens your senses.</p>
                                 <button onClick={handleViewMenu} className="px-5 py-2 font-semibold text-sm bg-primary text-white hover:bg-white hover:text-primary active:scale-95 transition-all">VIEW MENU</button>
                             </div>
                         </div>
--- src/components/main-layout/Navbar.jsx
+++ src/components/main-layout/Navbar.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
 import logo from "../../assets/logo/chefs-domain-logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
     const links = <>
-    <li>HOME</li>
-    <li>FOOD ITEMS</li>
-    <li>BLOG</li>
+        <li>HOME</li>
+        <li>FOOD ITEMS</li>
+        <li>BLOG</li>
     </>
 
     return (
         <>
             {/* large device */}
-            <div className="absolute flex justify-between items-center text-white px-10 py-2 text-sm font-semibold z-10 w-full max-w-screen-2xl">
+            <div className="hidden absolute lg:flex justify-between items-center text-white px-10 py-2 text-sm font-semibold z-10 w-full max-w-screen-2xl">
                 <div className="flex-[1] text-left">
                     <img className="w-40 max-w-[33%]" src={logo} alt="" />
                 </div>
@@ -22,9 +27,25 @@ const Navbar = () => {
                 </div>
 
                 <div className="flex-[1] text-right">
-                    <button className="px-5 py-2 font-bold bg-white text-primary hover:bg-primary hover:text-white">LOGIN</button>
+                    <button className="px-5 py-2 font-bold bg-white text-primary hover:bg-primary hover:text-white active:scale-95 transition-all">LOGIN</button>
                 </div>
             </div>
+
+            {/* medium device */}
+            <div className={`absolute z-10 flex items-center justify-between lg:hidden md:px-10 px-5 py-2 text-white w-full`}>
+                <div className="flex sm:gap-5 gap-2 items-center">
+                    <FiMenu onClick={() => setShowMenu(!showMenu)} className="text-2xl cursor-pointer" />
+                    <Link to='/'><img className="h-14 max-w-full cursor-pointer" src={logo} alt="" /></Link>
+                </div>
+                <div className={`${showMenu ? 'flex' : 'hidden'} flex-col absolute top-16 bg-[#000000BB] px-8 py-5 rounded space-y-3 z-10 text-white list-none`}>
+                    {
+                        links
+                    }
+                </div>
+                <div className="flex items-center gap-10">
+                    <button className="px-5 py-2 font-bold bg-white text-sm text-primary hover:bg-primary hover:text-white active:scale-95 transition-all">LOGIN</button>
+                </div >
+            </div >
         </>
     );
 };
